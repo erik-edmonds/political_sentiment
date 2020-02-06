58ec43833491f4239b83b63faa3df94ebaa7f0ea
erik-edmonds <erik.edmonds@daugherty.com>
2020-02-06 14:17:42 -0600

    in-flight

--- bin/mongo_connector.sh
+++ bin/mongo_connector.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-curl -X PUT localhost:8083/connectors/mongo_connector/config -H "Content-Type: application/json" -d '{
-	"name": "mongo_connector",
+curl -X POST http://localhost:8083/connectors -H "Content-Type: application/json" -d '{
+	"name":"mongo_connector",
 	"config": {
-		"connector.class":"MongoSourceConnector",
-		#"key.converter":"org.apache.kafka.connect.storage.StringConverter",
-		#"value.converter":"org.apache.kafka.connect.storage.StringConverter",
-		"connection.uri": "mongodb://erikedmonds:Edmonds25@localhost:27017",
-		"database":"test_database",
-		"collection":"test_collection",
-		"topic.prefix":""
+      		"tasks.max":1,
+      		"connector.class":"com.mongodb.kafka.connect.MongoSourceConnector",
+      		"key.converter":"org.apache.kafka.connect.storage.StringConverter",
+      		"value.converter":"org.apache.kafka.connect.storage.StringConverter",
+      		"connection.uri":"mongodb://erikedmonds:Edmonds25@localhost:27017",
+      		"database":"test_database",
+      		"collection":"test_collection",
+      		"pipeline":"[{\"$match\": { \"$and\": [ { \"updateDescription.updatedFields.quantity\" : { \"$lte\": 5 } }, {\"operationType\": \"update\"}]}}]", 
+		"topic.prefix": ""
 	}
 }'
